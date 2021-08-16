a8a568b6012d3ba8a2d877bd76e39dea17751776
Kenneth Murerwa <murerwa@Kenneths-MacBook-Air.local>
2021-08-16 17:52:18 +0300

    feat: Initial Commit

---
+++ app/src/main/java/com/murerwa/composetutorial/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.murerwa.composetutorial
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.murerwa.composetutorial.ui.theme.ComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Green)
+                    .padding(10.dp)
+            ) {
+                Text(text = "Hello")
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(text = "World")
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        ComposeTutorialTheme {
+
+        }
+    }
+}
